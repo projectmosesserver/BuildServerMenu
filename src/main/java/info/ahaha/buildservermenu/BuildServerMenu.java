fd593121024430a170328b18e6fc85b1dd7730f7
tsuoihito <tsuoihito@outlook.jp>
2022-11-17 21:09:34 +0900

    fix: イベントの登録し忘れ

--- src/main/java/info/ahaha/buildservermenu/BuildServerMenu.java
+++ src/main/java/info/ahaha/buildservermenu/BuildServerMenu.java
@@ -1,8 +1,10 @@
 package info.ahaha.buildservermenu;
 
 import info.ahaha.buildservermenu.cmd.Cmd;
+import info.ahaha.buildservermenu.listener.MenuCloseListener;
 import info.ahaha.buildservermenu.listener.MenuListener;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -15,7 +17,9 @@ public final class BuildServerMenu extends JavaPlugin {
     @Override
     public void onEnable() {
         getCommand("menu").setExecutor(new Cmd(this));
-        getServer().getPluginManager().registerEvents(new MenuListener(this), this);
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(new MenuListener(this), this);
+        pm.registerEvents(new MenuCloseListener(this), this);
     }
 
     @Override
